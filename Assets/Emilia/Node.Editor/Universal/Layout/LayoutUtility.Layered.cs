59cbf0f153f2144a88b501d3dc64ab951f91ebb3
EMT <1076995595@qq.com>
2024-12-27 16:18:06 +0800

    布局调整

---
+++ Assets/Emilia/Node.Editor/Universal/Layout/LayoutUtility.Layered.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Emilia.Kit;
+using Emilia.Node.Editor;
+using Microsoft.Msagl.Core.Geometry.Curves;
+using Microsoft.Msagl.Core.Layout;
+using Microsoft.Msagl.Core.Routing;
+using Microsoft.Msagl.Layout.Layered;
+using UnityEngine;
+
+namespace Emilia.Node.Universal.Editor
+{
+    public static partial class LayoutUtility
+    {
+        /// <summary>
+        /// 层级布局
+        /// </summary>
+        public static void LayeredLayout(EditorGraphView graphView, SugiyamaLayoutSettings layoutSettings = null, float time = 0.5f)
+        {
+            GeometryGraph layoutGraph = new GeometryGraph();
+
+            SetGraph(graphView, layoutGraph);
+
+            if (layoutSettings == null)
+            {
+                layoutSettings = new SugiyamaLayoutSettings {
+                    Transformation = PlaneTransformation.Rotation(Math.PI / 2),
+                    EdgeRoutingSettings = {EdgeRoutingMode = EdgeRoutingMode.StraightLine},
+                    NodeSeparation = 20,
+                    LayerSeparation = 50,
+                };
+            }
+
+            LayeredLayout layout = new LayeredLayout(layoutGraph, layoutSettings);
+
+            graphView.window.ShowNotification(new GUIContent("正在计算布局"));
+
+            Task.Run(OnLayout);
+
+            void OnLayout()
+            {
+                layout.Run();
+
+                EditorKit.UnityInvoke(() => graphView.window.RemoveNotification());
+
+                SetPosition(graphView, layoutGraph, time);
+            }
+        }
+    }
+}
---
+++ Assets/Emilia/Node.Editor/Universal/Layout/LayoutUtility.Tree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Emilia.Kit;
+using Emilia.Node.Editor;
+using Microsoft.Msagl.Core.Geometry.Curves;
+using Microsoft.Msagl.Layout.Layered;
+using Microsoft.Msagl.Miscellaneous;
+using Microsoft.Msagl.Prototype.Phylo;
+using UnityEngine;
+
+namespace Emilia.Node.Universal.Editor
+{
+    public static partial class LayoutUtility
+    {
+        /// <summary>
+        /// 树形布局
+        /// </summary>
+        public static void TreeLayout(EditorGraphView graphView, SugiyamaLayoutSettings layoutSettings = null)
+        {
+            PhyloTree tree = new PhyloTree();
+
+            SetGraph(graphView, tree, edgeFunc: () => new PhyloEdge(null, null));
+
+            if (layoutSettings == null)
+            {
+                layoutSettings = new SugiyamaLayoutSettings() {
+                    Transformation = PlaneTransformation.Rotation(Math.PI),
+                    NodeSeparation = 20,
+                    LayerSeparation = 50,
+                };
+            }
+
+            graphView.window.ShowNotification(new GUIContent("正在计算布局"));
+
+            Task.Run(OnLayout);
+
+            void OnLayout()
+            {
+                LayoutHelpers.CalculateLayout(tree, layoutSettings, null);
+
+                EditorKit.UnityInvoke(() => graphView.window.RemoveNotification());
+
+                SetPosition(graphView, tree, 0.5f);
+            }
+        }
+    }
+}
